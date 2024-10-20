c17cca79962226954630f225ef2cf1ff1e94938e
aamandaps <aamandaps01@gmail.com>
2024-10-19 21:37:23 -0300

    Add arquivos ao projeto

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace MauiAppLogin
+﻿
+namespace MauiAppLogin
 {
     public partial class App : Application
     {
@@ -8,5 +9,16 @@
 
             MainPage = new AppShell();
         }
-    }
-}
+        //Método pra alinhar a abertura do app
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+        }
+
+    }//Fechamento da classe
+}//Fechamento namespace
